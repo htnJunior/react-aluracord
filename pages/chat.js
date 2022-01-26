112b33b47fddfc6afb4ccc9e4c63f08215d7177d
hso <hso@certi.org.br>
2022-01-26 14:10:09 -0300

    segundo dia de imersao alura

---
+++ pages/chat.js
@@ -0,0 +1,7 @@
+const Chat = (props) => {
+    return (
+        <h1>Página do chat</h1>
+    )
+}
+
+export default Chat
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -47,11 +20,23 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  const [username, setUsername] = useState('')
+
+  const router = useRouter()
+
+  const inputUsername = e => { // text input handler
+    const value = e.target.value
+    setUsername(value)
+  }
+
+  const getImageUrl = username => 
+        username.length >= 3 ? `http://github.com/${username}.png` : ''
+
+  const userNameValidate = username => 
+      username.length >= 3 ? username : ''      
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -78,17 +63,23 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(e) => {
+              e.preventDefault()
+              router.push('./chat')
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
+            <Titulo tag="h2">Welcome!</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
 
             <TextField
+              value={username}
+              onChange={inputUsername}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -135,7 +126,7 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+              src={getImageUrl(username)}
             />
             <Text
               variant="body4"
@@ -146,7 +137,7 @@ export default function PaginaInicial() {
                 borderRadius: '1000px'
               }}
             >
-              {username}
+              {userNameValidate(username)}
             </Text>
           </Box>
           {/* Photo Area */}
